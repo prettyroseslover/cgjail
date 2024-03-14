92beaf696b4da68c84ae48d4439db398dc81dade
prettyroseslover <potapova.polia2002@yandex.ru>
2024-03-14 19:54:48 +0300

    add new arguments

--- gather/gather.py
+++ gather/gather.py
@@ -1,15 +1,40 @@
 # how to beautify that? 
-import sys
+import sys, os
 sys.path.insert(0, '../')
 
-from parse import pid
+from parse import pid, period, docile, naughty, verbose
 from config import path_to_storage
 from colored_text import colored
+
+def count_cpu_usage(pid):
+
+    clock_tick = os.sysconf(os.sysconf_names['SC_CLK_TCK'])
+
+    with open(f"/proc/{pid}/stat", "r") as f:
+        stat = f.readline().strip('\n').split(" ")
+    
+    with open("/proc/uptime", "r") as f:
+        system_uptime_sec = int(float(f.readline().strip('\n').split(" ")[0]))
+    
+    process_utime_sec = int(stat[13]) / clock_tick
+    process_stime_sec = int(stat[14]) / clock_tick
+    process_starttime_sec = int(stat[21]) / clock_tick
+
+    process_elapsed_sec = system_uptime_sec - process_starttime_sec
+    process_usage_sec = process_utime_sec + process_stime_sec
+    proess_usage = process_usage_sec * 100 / process_elapsed_sec
+
+    return (process_usage_sec, proess_usage)
+
 
 # colored.print_cyan(path_to_storage)
 with open(f"/proc/{pid}/status") as f:
     process_name = f.readline().strip('\n').split(":\t")[1]
 
-colored.print_cyan(f"{pid} {process_name}")
+# total = 16294880 
 
-total = 16294880 
+if verbose:
+    colored.print_cyan(f"{pid} {process_name}")
+    colored.print_cyan(f"Conduct survey {docile} + {naughty} times each {period} seconds")
+    res = count_cpu_usage(pid)
+    colored.print_cyan(f"{res[0]} {res[1]}")
--- gather/parse/__init__.py
+++ gather/parse/__init__.py
@@ -13,9 +13,23 @@ def pid(x):
         raise ValueError('No such PID')
     return pid
 
+def positive_int(x):
+    n = int(x)
+    if n <= 0:
+        raise ValueError('Negative values are not allowed')
+    return n
+
 parser = CustomArgumentParser(description='Script to prepare data and train kNN-model for a given process')
 parser.add_argument('-p', '--pid', required=True, type=pid, help='PID of a process')
+parser.add_argument('-T', '--period', default=5, type=positive_int, help='Survey period in seconds')
+parser.add_argument('-d', '--docile', default=50, type=positive_int, help='Number of data point to gather per docile process')
+parser.add_argument('-n', '--naughty', default=50, type=positive_int, help='Number of data point to gather per naughty process')
+parser.add_argument('-v', '--verbosity', action = "store_true")
 
 args = parser.parse_args()
 
 pid = args.pid
+period = args.period
+docile = args.docile
+naughty = args.naughty
+verbose = args.verbosity
