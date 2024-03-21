afe6a3a92e7580cd94b3395e7ed8fb0e7d1a1454
prettyroseslover <potapova.polia2002@yandex.ru>
2024-03-21 17:06:20 +0300

    add first level children

--- gather/gather.py
+++ gather/gather.py
@@ -4,26 +4,32 @@ sys.path.insert(0, '../')
 
 from parse import pid, period, docile, naughty, verbose
 from metrics.cpu import momentary_cpu_usage
+from metrics.children import children
 from metrics.memory import pss_bytes, rss_bytes, memtotal_bytes, mem_top
 from config import path_to_storage
 from colored_text import colored
 
 
-# colored.print_cyan(path_to_storage)
 with open(f"/proc/{pid}/status") as f:
     process_name = f.readline().strip('\n').split(":\t")[1]
 
+
 if verbose:
-    colored.print_cyan(f"{pid} {process_name}")
+    #colored.print_cyan(f"{pid} {process_name}")
     # colored.print_cyan(f"Conduct survey {docile} + {naughty} times each {period} seconds")
 
-    colored.print_cyan(pss_bytes(pid))
-    colored.print_cyan(rss_bytes(pid))
-    colored.print_cyan(memtotal_bytes())
+    #colored.print_cyan(pss_bytes(pid))
+    #colored.print_cyan(rss_bytes(pid))
+    #colored.print_cyan(memtotal_bytes())
 
-    print(mem_top(pid) * 100)
+    #print(mem_top(pid) * 100)
 
     '''
     for i in range(10):
         colored.print_cyan(f"{momentary_cpu_usage(pid)}")
     '''
+     
+    
+    colored.print_cyan(children(pid))
+
+
--- gather/metrics/children.py
+++ gather/metrics/children.py
@@ -1,2 +1,18 @@
-def children():
-    return 0
+import os
+
+def children(pid):
+    
+    tids = os.listdir(f"/proc/{pid}/task")
+    all_children = []
+
+    for tid in tids:
+        with open(f"/proc/{pid}/task/{tid}/children") as f:
+            children = f.readline().strip(' ').split(' ')
+        
+        if children == ['']:
+            continue
+
+        all_children.extend(list(map(int, children)))
+
+    return all_children
+
