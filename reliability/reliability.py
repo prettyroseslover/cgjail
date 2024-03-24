5579610c3238b89e91e391b9cc8804a0212fc757
prettyroseslover <potapova.polia2002@yandex.ru>
2024-03-24 18:55:40 +0300

    add simple docile/naughty scenario

--- reliability/reliability.py
+++ reliability/reliability.py
@@ -5,11 +5,11 @@ from config import path_to_storage
 from colored_text import colored
 from gather import df_generator
 
-#import pandas as pd
+if verbose:
+    colored.print_cyan(f"==ON THE {args.step} STEP==")
 
 if args.step == 'train':
     csv_file = args.input
-    print(f"TRAIN: {csv_file}")
     exit()
 
 process_class = int(args.step != 'docile')
@@ -21,17 +21,21 @@ with open(f"/proc/{pid}/status") as f:
     process_name = f.readline().strip('\n').split(":\t")[1]
 
 if verbose:
-    colored.print_cyan(f"==ON THE {args.step} STEP==")
     colored.print_cyan("You've chosen process:")
     colored.print_cyan(f"{pid} {process_name}")
 
 
-csv_file = f"{path_to_storage}/{process_name}.csv"
 df = df_generator(pid, number, period, process_class)
 
 if verbose:
     print(df.head())
 
+if args.step == 'naughty':
+    output = args.output
+    df.to_csv(output, mode='a', index=False, header=False)
+    colored.print_green(f"Results are appended to {output}")
+    exit()
 
+csv_file = f"{path_to_storage}/{process_name}.csv"
 df.to_csv(csv_file, index=False)
 colored.print_green(f"Results are written to {csv_file}")
