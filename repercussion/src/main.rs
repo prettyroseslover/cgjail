bee68e46f0b4dca4247b5f5c5f6afe10db4166fc
prettyroseslover <potapova.polia2002@yandex.ru>
2024-04-30 16:20:40 +0300

    move cgjail to the fucntion

--- repercussion/src/main.rs
+++ repercussion/src/main.rs
@@ -1,21 +1,20 @@
-#![allow(dead_code)]
-#![allow(unused_variables)]
-#![allow(unused_imports)]
-
 use color_eyre::{eyre::eyre, Report, Result};
 use serde::Deserialize;
-use std::collections::HashMap;
-use std::ffi::OsStr;
-use std::{fs, env};
-use std::io::{BufRead, BufReader};
-use std::path::{Path, PathBuf};
+use std::{
+    collections::HashMap,
+    env,
+    ffi::OsStr,
+    fs,
+    io::{BufRead, BufReader},
+    path::{Path, PathBuf},
+};
 use toml;
 
-use pyo3::prelude::*;
-use pyo3::types::{PyAny, PyList};
-
-use cgroups_rs::*;
-use cgroups_rs::cgroup_builder::*;
+use cgroups_rs::{cgroup_builder::*, *};
+use pyo3::{
+    prelude::*,
+    types::{PyAny, PyList},
+};
 
 #[derive(Deserialize, Debug)]
 struct Config {
@@ -89,7 +88,7 @@ fn which_to_monitor(pickles: &HashMap<usize, String>) -> Result<HashMap<u64, (St
         })
         .collect();
 
-    return Ok(processes_to_monitor)
+    return Ok(processes_to_monitor);
 }
 
 fn import_model<'py>(path_to_model: &String, py: Python<'py>) -> PyResult<&'py PyAny> {
@@ -98,13 +97,56 @@ fn import_model<'py>(path_to_model: &String, py: Python<'py>) -> PyResult<&'py P
     Ok(total)
 }
 
+fn cgjail(pid: u64, process_name: String, id: usize, models: &HashMap<usize, &PyAny>) -> Result<()> {
+    let jail_name = format!("{}_jail", process_name);
+
+    let cpu_usage_quota = models[&id].getattr("cpu_usage_quota")?.extract::<i64>()?;
+    let mem_max_bytes = models[&id].getattr("mem_max_bytes")?.extract::<i64>()?;
+    let maximum_number_of_processes = models[&id]
+        .getattr("maximum_number_of_processes")?
+        .extract::<i64>()?;
+
+    // get hierarchy (V2 or V1)
+    let hier = cgroups_rs::hierarchies::auto();
+
+    // creation of a cgroup wuth given limits
+    let cgjail: Cgroup = CgroupBuilder::new(&jail_name)
+        .memory()
+        .memory_hard_limit(mem_max_bytes) // limit is i64, bytes
+        .done()
+        .cpu()
+        .quota(cpu_usage_quota) // quota is i64
+        .done()
+        .pid()
+        .maximum_number_of_processes(MaxValue::Value(maximum_number_of_processes)) // value is i64
+        .done()
+        .build(hier)?;
+
+    let cpus: &cgroups_rs::cpu::CpuController = cgjail
+        .controller_of()
+        .ok_or(eyre!("Something went wrong in a CpuController"))?;
+    cpus.add_task_by_tgid(&CgroupPid::from(pid))?;
+
+    let memories: &cgroups_rs::memory::MemController = cgjail
+        .controller_of()
+        .ok_or(eyre!("Something went wrong in a MemController"))?;
+    memories.add_task(&CgroupPid::from(pid))?;
+
+    let pids: &cgroups_rs::pid::PidController = cgjail
+        .controller_of()
+        .ok_or(eyre!("Something went wrong in a PidController"))?;
+    pids.add_task(&CgroupPid::from(pid))?;
+
+    Ok(())
+}
+
 fn main() -> Result<()> {
     // the path to config_file is hardcoded
 
     let mut config_file_path = env::current_dir()?;
     config_file_path.pop();
     config_file_path.push("config/cgjailConfig.toml");
-    
+
     let config_file = fs::read_to_string(config_file_path)?;
 
     let config: Config = toml::from_str(&config_file)?;
@@ -143,8 +185,8 @@ fn main() -> Result<()> {
         .map(|(&id, pickle)| (id, format!("{}/{}", model_storage_as_string, pickle)))
         .collect();
 
-    println!("{:#?}", pickles);
-    println!("{:#?}", models_to_load);
+    // println!("{:#?}", pickles);
+    // println!("{:#?}", models_to_load);
 
     let mut python_path = env::current_dir()?;
     python_path.push("predict.py");
@@ -165,9 +207,10 @@ fn main() -> Result<()> {
             let syspath = py
                 .import_bound("sys")?
                 .getattr("path")?
-                .downcast_into::<PyList>().map_err(|e| eyre!("{}", e))?;
+                .downcast_into::<PyList>()
+                .map_err(|e| eyre!("{}", e))?;
             syspath.insert(0, &python_path.parent().map(|p| p.parent()))?;
-            
+
             // loop through every process to monitor
             for (pid, (process_name, id)) in processes_to_monitor.into_iter() {
                 println!("{}: {} {}", pid, process_name, id);
@@ -176,43 +219,13 @@ fn main() -> Result<()> {
                     .getattr("predict")?
                     .into();
                 let result = app.call1(py, (pid, models[&id]))?.extract::<u32>(py)?;
-                
+
                 if result == 1 {
-                    let jail_name = format!("{}_jail", process_name);
-
-                    let cpu_usage_quota = models[&id].getattr("cpu_usage_quota")?.extract::<i64>()?;
-                    let mem_max_bytes = models[&id].getattr("mem_max_bytes")?.extract::<i64>()?;
-                    let maximum_number_of_processes = models[&id].getattr("maximum_number_of_processes")?.extract::<i64>()?;
-                    
-                    // get hierarchy (V2 or V1)
-                    let hier = cgroups_rs::hierarchies::auto();
-
-                    // creation of a cgroup wuth given limits
-                    let cgjail: Cgroup = CgroupBuilder::new(&jail_name)
-                        .memory()
-                            .memory_hard_limit(mem_max_bytes) // limit is i64, bytes
-                            .done()
-                        .cpu()
-                            .quota(cpu_usage_quota) // quota is i64
-                            .done()
-                        .pid()
-                            .maximum_number_of_processes(MaxValue::Value(maximum_number_of_processes)) // value is i64
-                            .done()
-                        .build(hier)?;
-                    
-                    let cpus: &cgroups_rs::cpu::CpuController = cgjail.controller_of().ok_or(eyre!("Something went wrong in a CpuController"))?;
-                    cpus.add_task_by_tgid(&CgroupPid::from(pid))?;
-                    
-                    let memories: &cgroups_rs::memory::MemController = cgjail.controller_of().ok_or(eyre!("Something went wrong in a MemController"))?;
-                    memories.add_task(&CgroupPid::from(pid))?;
-
-                    let pids: &cgroups_rs::pid::PidController = cgjail.controller_of().ok_or(eyre!("Something went wrong in a PidController"))?;
-                    pids.add_task(&CgroupPid::from(pid))?;
-
+                    cgjail(pid, process_name, id, &models)?;
                 }
             }
             // for testing only run once
-            break
+            break;
         }
 
         Ok(())
